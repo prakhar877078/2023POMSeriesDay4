521f9711d14c8f3f372184e8ac28ad97a294999b
prakhar877078 <prakharpandey73@gmail.com>
2023-05-30 23:12:58 +0530

    ProductPageInfoTest modified

--- src/test/java/com/qa/opencart/tests/ProductPageInfoTest.java
+++ src/test/java/com/qa/opencart/tests/ProductPageInfoTest.java
@@ -1,6 +1,9 @@
 package com.qa.opencart.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import com.qa.opencart.base.BaseTest;
 
@@ -9,6 +12,24 @@ public class ProductPageInfoTest extends BaseTest{
 	@BeforeClass
 	public void productInfoPageSetup() {
 		accPage = loginpage.doLogin(prop.getProperty("username").trim(),prop.getProperty("password").trim());
+	}
+	
+	@DataProvider
+	public Object[][] getProductImagesTestData(){
+		return new Object[][] {
+			{"Macbook", "MacBook Pro", 4},
+			{"iMac", "iMac", 3},
+			{"Apple", "Apple Cinema 30\"", 6},
+			{"Samsung", "Samsung SyncMaster 941BW", 1},
+		};
+	}
+	
+	@Test(priority = 1, dataProvider = "getProductImagesTestData")
+	public void productImagesCountTest(String searchKey, String productName, int imagesCount) {
+		searchPage = accPage.performSearch(searchKey);
+		productInfoPage = searchPage.selectProduct(productName);
+		int actImagesCount = productInfoPage.getProductImagesCount();
+		Assert.assertEquals(actImagesCount, imagesCount);
 	}
 
 }
